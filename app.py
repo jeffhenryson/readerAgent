b56d2ec19aa2cdbc2f88965385b2d39200dc3be6
jeffhenryson <jeffhunbruey@gmail.com>
2024-02-23 11:21:11 -0300

    adicionando os pips do requirements tirando o try/catch do app

--- app.py
+++ app.py
@@ -8,16 +8,11 @@ mapper = CategoryURLMapper()
 
 @app.route('/read-url', methods=['POST'])
 def read_url():
-    
-    try:
+
         input_data = CategoriesReaderToolInput(**request.json)
         category = input_data.category
         urls = mapper.get_urls_by_category(category)
-        return jsonify(urls)
-    except ValidationError as e:
-        return jsonify({"error": str(e)}), 400
-    
-    
+        return jsonify(urls)    
     
 
 if __name__ == '__main__':
